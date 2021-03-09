08074990e7bb9d9292a3d92175a8a794881ff86b
srmacario <srgioreinier@gmail.com>
2021-03-09 00:35:19 -0300

    better merge

--- semana2/exemplos/merge_sort.cpp
+++ semana2/exemplos/merge_sort.cpp
@@ -2,50 +2,44 @@
 
 using namespace std;
 
+#define st first
+#define nd second
+#define pb push_back
+#define cl(x,v) memset((x), (v), sizeof(x))
+#define db(x) cerr << #x << " == " << x << endl
+#define dbs(x) cerr << x << endl
+#define _ << ", " <<
 
-int arr[] = {2, 8, 6, 10, 4, 3, 7, 5, 9, 11}; 
-int L[10000], R[100000];
+typedef long long ll;
+typedef long double ld;
+typedef pair<int,int> pii;
+typedef pair<int, pii> piii;
+typedef pair<ll,ll> pll;
+typedef pair<ll, pll> plll;
+typedef vector<int> vi;
+typedef vector <vi> vii;
 
-void merge(int l, int r){
-    //Condição de Parada: vetor de um elemento, não precisa ordenar
-    if(l == r) return;
-    //separando os tamanhos
-    int m = (l+r)/2, n1 = m-l+1, n2 = r-m;
-    //divide o problema atual em dois: lado esquerdo e lado direito
-    merge(l, m), merge(m+1, r);
-    //Nesse ponto, já resolveu os dois problemas anteriores, agora é necessário juntar os dois 
-    //Separando em dois para visualizar: arr está ordenado de l a m
-    //                                   arr está ordenado de m+1 a r
-    for(int i=0;i<n1;i++) L[i] = arr[l+i];
-    for(int i=0;i<n2;i++) R[i] = arr[m+1+i];
-    //anda com dois ponteiros verificando quem é o menor numero atual comparando entre os dois vetores (esquerda e direita)
-    int i = 0, j = 0, k = l;
-    while(i < n1 && j < n2){
-        if(L[i] <= R[j]) arr[k] = L[i], i++;
-        else arr[k] = R[j], j++;
-        k++;
+const ld EPS = 1e-9, PI = acos(-1.);
+const ll LINF = 0x3f3f3f3f3f3f3f3f;
+const int INF = 0x3f3f3f3f, MOD = 1e9+7;
+const int N = 1e5+5;
+
+ll cnt;
+
+vi merge(vi v){
+    int sz = (int)v.size();
+    if(sz == 1) return v;
+    vi l, r;
+    for(int i = 0; i < (sz)/2; i++) l.pb(v[i]);
+    for(int i = (sz)/2; i < sz; i++) r.pb(v[i]);
+    l = merge(l), r = merge(r);
+    int pl = 0, pr = 0;
+    vi ans;
+    while(pl < l.size() or pr < r.size()){
+        if(pl >= l.size()) ans.pb(r[pr++]);
+        else if(pr >= r.size()) ans.pb(l[pl++]);
+        else if(r[pr] >= l[pl]) ans.pb(l[pl++]);
+        else ans.pb(r[pr++]), cnt += l.size() - pl; //inversion
     }
-    //dependendo dos valores pode acabar esse loop sem ter passado totalmente pelo L ou totalmente pelo R
-    while(i<n1) arr[k] = L[i], i++, k++;
-    while(j<n2) arr[k] = R[j], j++, k++;
-    //fim dessa etapa do merge
+    return ans;
 }
-
-void print(int size) { 
-    for (int i=0; i < size; i++) 
-        printf("%d ", arr[i]); 
-    printf("\n"); 
-} 
-  
-int main() { 
-    int arr_size = sizeof(arr)/sizeof(arr[0]); 
-  
-    printf("Array original \n"); 
-    print(arr_size); 
-  
-    merge(0, arr_size - 1); 
-  
-    printf("\nArray ordenado \n"); 
-    print(arr_size); 
-    return 0; 
-} 
